da76bca5c0d5000d1b68ceb03384f9e5a01428b4
Achintya Rajan <rajan.achintya03@gmail.com>
2021-10-10 15:34:22 -0500

    login page

--- student/urls.py
+++ student/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from student import views
 
 urlpatterns = [
+    path('login/', views.login),
     path('', views.home)
 ]
--- student/views.py
+++ student/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'student/home.html')
+
+
+def login(request):
+    return render(request, 'student/login.html')
