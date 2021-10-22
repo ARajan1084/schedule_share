2f1f0d4f10dc516ed4838899c82e5e0bf6889c2f
m7yash <61370209+m7yash@users.noreply.github.com>
2021-10-21 19:58:32 -0700

    set up models for friendship, friend request, and enrollment

--- student/models.py
+++ student/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    first_name = models.CharField(max_length=30, null=False)
-    last_name = models.CharField(max_length=50, null=False)
+    first_name = models.CharField(max_length=30, unique=False, null=False)
+    last_name = models.CharField(max_length=50, unique=False, null=False)
 
     class Meta:
         db_table = 'students'
@@ -15,28 +15,41 @@ class Student(models.Model):
 
 class Class(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, null=False)
-    course_name = models.CharField(max_length=50, null=False)
-    teacher_first_name = models.CharField(max_length=30, null=False)
-    teacher_last_name = models.CharField(max_length=50, null=False)
+    course_name = models.CharField(max_length=50, unique=False, null=False)
+    teacher_first_name = models.CharField(max_length=30, unique=False, null=False)
+    teacher_last_name = models.CharField(max_length=50, unique=False, null=False)
     # sunday = 0
-    day = models.IntegerField(null=False)
-    start_time = models.TimeField(null=False)
-    end_time = models.TimeField(null=False)
+    day = models.IntegerField(unique=False, null=False)
+    start_time = models.TimeField(unique=False, null=False)
+    end_time = models.TimeField(unique=False, null=False)
 
     class Meta:
         db_table = 'classes'
 
 
 class Friendship(models.Model):
-    a = 1
-    # TODO: finish
+    # student can have multiple Friendships, and each Friendship involves multiple (specifically: 2) students
+    first_student_username = models.CharField(max_length=10, unique=False, null=False)
+    second_student_username = models.CharField(max_length=10, unique=False, null=False)
+
+    class Meta:
+        db_table = 'friendships'
 
 
 class FriendRequest(models.Model):
-    a = 1
-    # TODO: finish
+    # student can have multiple FriendRequests, but each individual FriendRequest is specific to one student
+    sending_student = models.CharField(max_length=10, unique=False, null=False)
+    receiving_student = models.CharField(max_length=10, unique=False, null=False)
+    # U = unknown, A = accept, R = reject
+    request_status = models.CharField(max_length=1, default='U', unique=False, null=False)
+
+    class Meta:
+        db_table = "friend requests"
 
 
 class Enrollment(models.Model):
-    a = 1
-    # TODO: finish
+    username = models.CharField(max_length=10, unique=False, null=False)
+    class_id = models.UUIDField(default=uuid.uuid4, primary_key=False, unique=False, null=False)
+
+    class Meta:
+        db_table = "enrollments"
