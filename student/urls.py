58b39dc55ccdc085d82822ee139eca114a0a85be
Achintya Rajan <rajan.achintya03@gmail.com>
2021-11-06 13:54:59 -0500

    update html files, added decorator

--- student/urls.py
+++ student/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('login/', views.login, name="login"),
     path('account/create/', views.create_account, name='create_account'),
     path('class/management/add/', views.add_class, name='add_class'),
-    path('class/management/remove/', views.add_class, name='remove_class'),
+    path('class/management/remove/', views.remove_class, name='remove_class'),
+    path('class/management/remove/<str:klass_id>/', views.remove_class, name='remove_a_class'),
     path('', views.home, name='home')
 ]
